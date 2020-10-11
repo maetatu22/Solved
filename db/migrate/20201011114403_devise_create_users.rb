839408e9f241cd29883bcf7b1f11f3dd5f22244b
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-12 01:25:23 +0900

    userテーブルにカラム追加

--- db/migrate/20201011114403_devise_create_users.rb
+++ db/migrate/20201011114403_devise_create_users.rb
@@ -4,9 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
+      t.string  :name,               null: false
+      t.text    :self_introduction
+      t.integer :genre_id
+      t.date    :birthday
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,10 @@
 ActiveRecord::Schema.define(version: 2020_10_11_114403) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "self_introduction"
+    t.integer "genre_id"
+    t.date "birthday"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
