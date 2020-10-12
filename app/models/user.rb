67f06614ec94302724be0cd412e52768beb06c80
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-12 10:52:15 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
   has_many :comments
   belongs_to_active_hash :gender
 
-
+  validates :name, presence: true
+  validates :self_introduction, length: { maximum: 160 }
+  validates :password ,format:  { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "Password Include both letters and numbers"}
 end
