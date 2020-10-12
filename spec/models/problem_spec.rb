66bbfb3b1c14c5fbd24c49488b7a7320c7e5df2c
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-12 21:08:05 +0900

    proglemモデル単体テストコード

--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Problem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @problem = FactoryBot.build(:problem)
+  end
+
+  describe '投稿の保存' do
+    context "投稿が保存できる場合" do
+      it "全項目があれば投稿が保存される" do
+        expect(@problem).to be_valid
+      end
+    end
+    context "投稿が保存できない場合" do
+      it "titleが空だと登録できない" do
+        @problem.title = ""
+        @problem.valid?
+        expect(@problem.errors.full_messages).to include("Title can't be blank")
+      end
+      it "titleが20文字より多いと登録できない" do
+        @problem.title = "1234567890123456789012"
+        @problem.valid?
+        expect(@problem.errors.full_messages).to include("Title is too long (maximum is 20 characters)")
+      end
+      it "textが空だと登録できない" do
+        @problem.text = ""
+        @problem.valid?
+        expect(@problem.errors.full_messages).to include("Text can't be blank")
+      end
+      it "textが14文字以下だと登録できない" do
+        @problem.text = "aaa"
+        @problem.valid?
+        expect(@problem.errors.full_messages).to include("Text is too short (minimum is 15 characters)")
+      end
+      it "textが200文字以上だと登録できない" do
+        @problem.text = "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+        @problem.valid?
+        expect(@problem.errors.full_messages).to include("Text is too long (maximum is 200 characters)")
+      end
+      it "genreが選択されていないと登録できない" do
+        @problem.genre_id = 1
+        @problem.valid?
+        expect(@problem.errors.full_messages).to include("Genre Select")
+      end
+    end
+
+  end
 end
